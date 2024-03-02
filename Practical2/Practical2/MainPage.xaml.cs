1efdca500ef5cb13eaeb218f61e53ff8d7600d33
Vlad <vsporyadin@gmail.com>
2024-03-02 23:05:29 +0300

    Добавьте файлы проекта.

---
+++ Practical2/Practical2/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Practical2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            logo.Source = ImageSource.FromResource("Practical2.vector.png");
+            btnCategory.ImageSource = ImageSource.FromResource("Practical2.category.png");
+            btnBag.ImageSource = ImageSource.FromResource("Practical2.bag.png");
+            arrowFull.Source = ImageSource.FromResource("Practical2.arrowfull.png");
+            arrowMt.Source = ImageSource.FromResource("Practical2.arrofmt.png");
+            arrowFull.Aspect = Aspect.Fill;
+            arrowMt.Aspect = Aspect.Fill;
+            item1.Source = ImageSource.FromResource("Practical2.itemone.png");
+            item2.Source = ImageSource.FromResource("Practical2.itemtwo.png");
+            grade.Source = ImageSource.FromResource("Practical2.grade.png");
+            grade2.Source = ImageSource.FromResource("Practical2.grade.png");
+            price1.Source = ImageSource.FromResource("Practical2.priceone.png");
+            price2.Source = ImageSource.FromResource("Practical2.pricetwo.png");
+            btnToCart.ImageSource = ImageSource.FromResource("Practical2.itemcart.png");
+            btnToCart2.ImageSource = ImageSource.FromResource("Practical2.itemcart.png");
+
+
+        }
+
+        private async void btnCategory_Clicked(object sender, EventArgs e)
+        {
+            Page2 page2 = new Page2(); 
+            Application.Current.MainPage = page2;
+        }
+    }
+}
---
+++ Practical2/Practical2/Page2.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Practical2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+            logo.Source = ImageSource.FromResource("Practical2.vector.png");
+            btnBack.ImageSource = ImageSource.FromResource("Practical2.back.png");
+            btnBag.ImageSource = ImageSource.FromResource("Practical2.bag.png");
+            item1.Source = ImageSource.FromResource("Practical2.itempagetwo.png");
+            grade.Source = ImageSource.FromResource("Practical2.grade.png");
+            itemBack.Source = ImageSource.FromResource("Practical2.itemback.png");
+            itemFront.Source = ImageSource.FromResource("Practical2.itemfront.png");
+            itemClose.Source = ImageSource.FromResource("Practical2.itemclose.png");
+            toWishList.Source = ImageSource.FromResource("Practical2.frame.png");
+            checkOut.Source = ImageSource.FromResource("Practical2.arrowfull.png");
+        }
+
+        private void btnBack_Clicked(object sender, EventArgs e)
+        {
+            MainPage MP = new MainPage();
+            Application.Current.MainPage = MP;
+        }
+    }
+}
