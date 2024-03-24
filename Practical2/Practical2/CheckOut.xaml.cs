c743afe53c0be433aab93f89bd5c3462eb5fa35b
Poryadinvs <vsporyadin@gmail.com>
2024-03-24 20:42:35 +0300

    addCheckoutPage

--- Practical2/Practical2/CheckOut.xaml.cs
+++ Practical2/Practical2/CheckOut.xaml.cs
@@ -17,6 +17,16 @@ namespace Practical2
             InitializeComponent();
             btnBack.ImageSource = ImageSource.FromResource("Practical2.back.png");
             btnBag.ImageSource = ImageSource.FromResource("Practical2.bag.png");
+            dots.Source = ImageSource.FromResource("Practical2.dots.png");
+            dots2.Source = ImageSource.FromResource("Practical2.dots.png");
+            dots3.Source = ImageSource.FromResource("Practical2.dots.png");
+            card.Source = ImageSource.FromResource("Practical2.card.png");
+            masterCard.ImageSource = ImageSource.FromResource("Practical2.MasterCard.png");
+            applePay.ImageSource = ImageSource.FromResource("Practical2.applePay.png");
+            payPal.ImageSource = ImageSource.FromResource("Practical2.PayPal.png");
+            visa.ImageSource = ImageSource.FromResource("Practical2.visa.png");
+            checkOut.Source = ImageSource.FromResource("Practical2.arrowfull.png");
+
         }
 
         private void btnBack_Clicked(object sender, EventArgs e)
